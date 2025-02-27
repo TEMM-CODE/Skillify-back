96373935200944c11b30a7cb706d0f1bbea5ef9f
raqlzz <raqlzz03@gmail.com>
2025-02-27 11:19:14 -0300

    adicinando métodos crate, update, getall, etc

--- src/main/java/com/temm/skillify/controller/UserControllerStudent.java
+++ src/main/java/com/temm/skillify/controller/UserControllerStudent.java
@@ -1,10 +1,14 @@
 package com.temm.skillify.controller;
 
+import com.temm.skillify.dto.UserRequestDto;
+import com.temm.skillify.dto.UserResponseDto;
 import com.temm.skillify.service.UserServiceStudent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/userStudent")
@@ -14,33 +18,29 @@ public class UserControllerStudent {
     private UserServiceStudent userServiceStudent;
 
     @PostMapping
-    public ResponseEntity create() {
-        userServiceStudent.create();
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto dto) {
+        UserResponseDto createdUser = userServiceStudent.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
-    @PutMapping
-    public ResponseEntity edit() {
-        userServiceStudent.edit();
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> edit(@PathVariable String id, @RequestBody UserRequestDto dto) {
+        UserResponseDto updatedUser = userServiceStudent.edit(id, dto);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @GetMapping
-    public ResponseEntity get() {
-        userServiceStudent.get();
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<List<UserResponseDto>> getAll() {
+        return ResponseEntity.ok(userServiceStudent.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getById() {
-        userServiceStudent.getById();
-        return ResponseEntity.status(HttpStatus.OK).build();
-
+    public ResponseEntity<UserResponseDto> getById(@PathVariable String id) {
+        return ResponseEntity.ok(userServiceStudent.getUserById(id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity delete() {
-        userServiceStudent.delete();
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/temm/skillify/model/repository/UserRepository.java
+++ src/main/java/com/temm/skillify/model/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.temm.skillify.model.repository;
 
+import com.temm.skillify.enums.TypeUser;
 import com.temm.skillify.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+    List<UserEntity> findByTypeUser(TypeUser typeUser);
 }
