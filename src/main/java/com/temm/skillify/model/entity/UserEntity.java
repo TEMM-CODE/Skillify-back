a895e420311154db9a74b95ae12faee2099d4175
raqlzz <raqlzz03@gmail.com>
2025-02-28 15:01:29 -0300

    criação e relacionamento da entidade UserImage

--- src/main/java/com/temm/skillify/model/entity/UserEntity.java
+++ src/main/java/com/temm/skillify/model/entity/UserEntity.java
@@ -2,13 +2,17 @@ package com.temm.skillify.model.entity;
 
 import com.temm.skillify.enums.TypeUser;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
 
     @SuppressWarnings("deprecation")
@@ -32,4 +36,100 @@ public class UserEntity {
     private Boolean studyReminder;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public TypeUser getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(TypeUser typeUser) {
+        this.typeUser = typeUser;
+    }
+
+    public Boolean getEmailNotification() {
+        return emailNotification;
+    }
+
+    public void setEmailNotification(Boolean emailNotification) {
+        this.emailNotification = emailNotification;
+    }
+
+    public Boolean getPushNotifications() {
+        return pushNotifications;
+    }
+
+    public void setPushNotifications(Boolean pushNotifications) {
+        this.pushNotifications = pushNotifications;
+    }
+
+    public Boolean getWeekReport() {
+        return weekReport;
+    }
+
+    public void setWeekReport(Boolean weekReport) {
+        this.weekReport = weekReport;
+    }
+
+    public Boolean getStudyReminder() {
+        return studyReminder;
+    }
+
+    public void setStudyReminder(Boolean studyReminder) {
+        this.studyReminder = studyReminder;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
---
+++ src/main/java/com/temm/skillify/model/entity/UserImage.java
@@ -0,0 +1,54 @@
+package com.temm.skillify.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserImage {
+
+    @SuppressWarnings("deprecation")
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(length = 36)
+    private String id; // Armazenamento do UUID como String
+
+    @OneToOne
+    private UserEntity user;
+
+    @Lob
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
