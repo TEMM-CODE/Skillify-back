c81b0857688a3cca505783cb5b7c1305366617a9
Julia <gulacer2@gmail.com>
2025-03-03 04:56:49 -0300

    feat: adicionar autorizador no swagger

--- src/main/java/com/temm/skillify/config/OpenApiConfig.java
+++ src/main/java/com/temm/skillify/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.temm.skillify.config;
 
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +14,11 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
-                        .title("My API")
-                        .version("1.0")
-                        .description("API documentation for my Spring Boot application"));
+                        .title("API Skillify")
+                        .description("Skillify API."));
     }
 }
