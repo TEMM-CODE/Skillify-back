5889cb79fdb2ae8ce535b41f201cd94086918bee
Julia <gulacer2@gmail.com>
2025-08-07 13:19:56 -0300

    cors fix

--- src/main/java/com/temm/skillify/config/SecurityConfig.java
+++ src/main/java/com/temm/skillify/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
+        configuration.setAllowedOrigins(Arrays.asList("https://skillify-ochre.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
